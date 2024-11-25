a1a7ca6001fc576d5e2f0af564da046ec7b90d30
Cloud User <ec2-user@ip-172-31-35-179.ap-southeast-2.compute.internal>
2024-11-25 16:04:47 +1000

    Dump sysctl stats to a file in stats_capture/ directory

--- dumpStats.sh
+++ dumpStats.sh
@@ -2,7 +2,13 @@
 
 # Script to continuously dump stats for a particular Java process
 #
+# Check out the following subscripts for details on what is captured:
+#     - dump_top.sh
+#     - dump_threads.sh
+#     - dump_jstack.sh
+#     - dump_vmstat.sh
 
+DUMP_DIR=stats_capture/
 
 # Check if a PID and a specified duration to capture the stats is provided
 if [ $# -ne 2 ]; then
@@ -10,11 +16,14 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
+# Capture the sysctl values
+sysctl -a > $DUMP_DIR/sysctl.out 2>&1
+
 # Do an initial heap dump of the java process
 rm -f stats_capture/heap_start.hprof
 jmap -dump:live,format=b,file=stats_capture/heap_start.hprof $1
 
-# Invoke the sub scripts
+# Invoke the sub scripts to run in the background
 sh dump_top.sh $2 &
 sh dump_threads.sh $1 $2 &
 sh dump_jstack.sh $1 $2 &
