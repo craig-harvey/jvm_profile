263876e85478ba4b1e33d86c7bb024bff48ca58c
Craig Harvey <crharvey0@gmail.com>
2024-12-11 22:51:06 +1000

    Added application CPU and MEM capture per second, tweaks

---
+++ dump_app_mem.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#
+# Uses `top` to dump % MEM utilisation for all jboss apps
+#
+# Due to `top` typically displaying each app on a separate line, some crazy awk was used
+# to consolidate the rows of CPU data from top into a single line.
+#
+# Output format:
+#
+#    Timestamp,app1,app2,app3
+#    t1,0.2,3.5,3.2
+#    t2,0.1,3.9,5.2
+#
+
+DURATION=$1
+
+if [ -z $DURATION ]
+  then
+    DURATION=1
+fi
+
+# Create the header by getting all the java apps by name and sorting them by PID
+ps -ef | grep java | sort -nk2 | grep -o '\[[^]]*\]' | grep -v pcid | awk '{printf ",%s", $0}' | awk '{printf "timestamp%s\n", $0}' > stats_capture/appmem.stats
+
+# Determine the number of Java apps we will be working with
+NUM_APPS=`top -b -n 1 -p $(pgrep -d, java) | grep jboss | wc -l`
+
+# In RHEL, the top command will print the stats since last invocation, which could be a while,
+# So we use top -n 2 and delete the first set of rows returned (Which is equal to the number of java apps)
+SED_CMD="1,${NUM_APPS}d"
+
+#
+# Loop for every second until it reaches the desired execution time
+#
+for i in $(seq 1 $1 )
+do
+  DATE=`date +%s | tr -d \"\n\"`
+  top -b -n 2 -p $(pgrep -d, java) |  grep jboss | sed $SED_CMD | sort -nk1 | eval "awk 'NR % $NUM_APPS == 1 {printf \"%s\", $DATE} {printf \",%s\", \$10} NR % $NUM_APPS == 0 {printf \"\\n\"}'" >> stats_capture/appmem.stats
+done
+
--- dump_top.sh
+++ dump_top.sh
@@ -1,16 +1,19 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Script to continuously monitor TOP dumping output to a file in CSV format
 #
 # Input - Takes a variable for the number of seconds to record for, otherwise runs top once and exits.
 
-# Stats Captured:
-#       - timestamp
-#       TOP stats:
+# Output 
+# 
+#   top.stats: Capture system level stats from the first 5 lines from `top`, prepending a unix timestamp to each line.
+#
+#   stats captured:
 #       - Load average (for the last minute)
 #       - CPU: user, system, idle, wait, hw_interrupts, sw_interrupts, physical_cpu_wait (Note: We omit ni)
 #       - MEM: total, free, used, buffered
 #       - SWAP: total, free, used, available
+#
 
 DURATION=$1
 
@@ -24,16 +27,15 @@ echo "Timestamp,load_average,cpu_user,cpu_system,cpu_idle,cpu_wait,cpu_hw_interr
 
 ## Print the required fields from top  (See Comment above)
 ## NB: The below awk column numbers may change depending on distro
-PRINT_LOAD='NR==1{printf ",%s", $12}'
+PRINT_LOAD='NR==1{printf "%s,%s", timestamp, $12}'
 PRINT_CPU='NR==3{printf "%s,%s,%s,%s,%s,%s,%s,",$2,$4,$8,$10,$12,$14,$16}'
 PRINT_MEM='NR==4{printf "%s,%s,%s,%s,",$4,$6,$8,$10}'
 PRINT_SWP='NR==5{print $3","$5","$7","$9}'
 
-AWK_SCRIPT="awk 'BEGIN {system(\"date +%s | tr -d \\\"\n\\\"\")} $PRINT_LOAD $PRINT_CPU $PRINT_MEM $PRINT_SWP ' >> stats_capture/top.stats"
-
 for i in  $( seq 1 $1 )
 do
-  top -b -n 1 | eval $AWK_SCRIPT
+  DATE=`date +%s | tr -d \"\n\"`
+  top -b -n 1 | eval "awk -v timestamp=$DATE ' $PRINT_LOAD $PRINT_CPU $PRINT_MEM $PRINT_SWP' >> stats_capture/top.stats"
   sleep 1
 done
 
